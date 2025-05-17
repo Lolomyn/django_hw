c0aec3d9e2e3f862823c8aad584985e25d7d36be
Savelev Maxim <vismanmark@yandex.ru>
2025-05-17 15:42:02 +0300

    do main part of hw

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='photos/', verbose_name='Превью')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
+    count_of_views = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return f"{self.title}: {self.content}./{self.created_at}"
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['title', 'created_at']
--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('product_detail/<int:pk>', views.product_detail, name='product_detail')
+    path('home/', views.ProductListView.as_view(), name='home'),
+    path('contacts/', views.ContactsCreateView.as_view(), name='contacts'),
+    path('product_detail/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail')
 ]
